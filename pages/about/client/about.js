a29fd40015200e0d2aede0bdda81d3ef65aa6c97
Rong GUAN <tianbao971@gmail.com>
2017-07-02 21:52:57 -0400

    Add speaker to "about" page

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,46 +1,13 @@
-// Session.set('voices',window.speechSynthesis.getVoices());
-// voices = [];
-// theVoice=null;
-//
-// Template.talk.helpers({
-// 	theVoices: function(){console.log("v="+Session.get('voices')); return voices;}
-// })
-//
-//
-// Template.talk.events({
-// 	'click .talk': function(event){
-// 		console.log("in the click talk event");
-// 		var msg = new SpeechSynthesisUtterance('Hello World');
-// 		if (theVoice) msg.voice=theVoice;
-// 		window.speechSynthesis.speak(msg);
-// 	},
-//
-// 	'click .say': function(event){
-// 		//var text = $("#words").val();
-// 		var msg = new SpeechSynthesisUtterance(words.value);
-// 		if (theVoice) msg.voice=theVoice;
-// 		window.speechSynthesis.speak(msg);
-// 	},
-//
-// 	'click .getVoices': function(event){
-// 		console.log("getting voices!");
-// 		voices= window.speechSynthesis.getVoices();
-// 		Session.set('voices',voices);
-// 		console.log("voices = "+JSON.stringify(voices));
-// 	},
-//
-// 	'change #voiceselect': function(event){
-// 		theVoice = this;
-// 		theEvent = event;
-// 		theVoice = voices[event.currentTarget.selectedIndex]
-// 		console.log("the voice index ="+event.currentTarget.selectedIndex);
-// 	}
-//
-// });
-//
-// Template.showvoice.events({
-// 	'click .voice': function(event){
-// 		theVoice = this;
-// 		console.log("the voice="+theVoice);
-// 	}
-// })
+Template.talk1.events({
+	'click button'(elt, instance){
+		var msg = new SpeechSynthesisUtterance('Joshua Alvarado is a rising junior pursuing his major in Computer Science. He is from Queens, New York City and has developed a love for coding during his time at Brandeis. Having once participated in Pito Salas’ class “Software Entrepreneurship”, he and his team worked on a startup named AmIFull, which served to provide businesses valuable metrics on their own customer tendencies, as well as their local competitors. He created a forum named “Mighty-IT”. Click here to join his forum');
+		window.speechSynthesis.speak(msg);
+	}
+})
+
+Template.talk4.events({
+	'click button'(elt, instance){
+		var msg = new SpeechSynthesisUtterance('Rong Guan is a Master student in Computer Science. She likes to develop different kinds of games. For example, console-based games such as memory card game and 3D Animation games such as "SuperApple" and "Hit Mushroom". You may check it out at her 3D Animation World. Rong received her Ph.D. degree in BioMedical Science at UT Southwestern Medical Center at Dallas. So she created a forum "From Bio to CS". Click here to join her forum.');
+		window.speechSynthesis.speak(msg);
+	}
+})
