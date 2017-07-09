f9e3f503e84478324828bed42e4fefb50c6695ee
YaxinGuo <yxguo0922@brandeis.edu>
2017-07-09 12:09:43 -0400

    update

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -1,22 +1,35 @@
 Template.home.onRendered(
   function() {
+    var myVar;
     var slideIndex = 0;
-    showSlides();
+    var slides;
+    var dots;
+
+    myFunction();
+
+    function myFunction(){
+      myVar = setInterval(showSlides, 3000);
+    }
 
     function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        for (i = 0; i < slides.length; i++) {
-           slides[i].style.display = "none";
+
+            var i;
+            slides = $(".mySlides");
+            dots = $(".dot");
+            for (i = 0; i < slides.length; i++) {
+               slides[i].style.display = "none";
+            }
+            slideIndex++;
+            //console.log("s"+slideIndex);
+            //console.log("sl"+slides.length);
+            //console.log("d"+dots.length);
+            if (slideIndex== slides.length) {slideIndex = 0}
+            //slides._slideTo( slides.index );
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace("active", "");
+            }
+            $(slides[slideIndex-1]).css("display", "block");
+            $(dots[slideIndex-1]).attr("class", $(dots[slideIndex-1]).attr("class") + " active");
+            //setTimeout(showSlides, 3000); // Change image every 2 seconds
         }
-        slideIndex++;
-        if (slideIndex> slides.length) {slideIndex = 1}
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
-        setTimeout(showSlides, 3000); // Change image every 2 seconds
-    }
-})
+    })
