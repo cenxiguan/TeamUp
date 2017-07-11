e4ef739171cdb2ddb8620d90ea690d3716766b91
beckettb <beckettb@brandeis.edu>
2017-07-11 11:26:51 -0400

    Revert "update"
    This reverts commit ea29042da66ef739bb738d059d493b5156e750e1.

--- pages/createGroup/client/createGroup.js
+++ pages/createGroup/client/createGroup.js
@@ -1,6 +1,5 @@
 Template.groupInfo.events({
   'click button'(elt,instance) {
-    console.log('test');
     const groupname = instance.$('#js-groupname').val();
     const groupdesc = instance.$('#js-groupdescription').val();
     const grouploc = instance.$('#js-grouplocation').val();
@@ -19,10 +18,11 @@ Template.groupInfo.events({
         groupdesc:groupdesc,
         grouploc:grouploc,
         owner:Meteor.userId()
+        // Need to implement groupid somehow which makes
+        // individual pages and unique groups
       };
 
     //first need to check if groupname is free, if not return error
     Meteor.call('groups.insert', groupinfo);
-
   }
 })
