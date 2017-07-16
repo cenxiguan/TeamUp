31aa70a960d32982d572fd2f4ef8574387a29988
Rong GUAN <tianbao971@gmail.com>
2017-07-15 23:44:00 -0400

    Add voice response to "calendar" page

--- pages/calendar/client/calendar.js
+++ pages/calendar/client/calendar.js
@@ -2,56 +2,47 @@
   This code comes from this blog post by Amit Agarwal
       http://ctrlq.org/code/19680-html5-web-speech-api
 */
-	var text = "";
-	var recognizing = false;
+var recognizing = false;
 
-	if ('webkitSpeechRecognition' in window) {
-		console.log("webkit is available!");
-		var recognition = new webkitSpeechRecognition();
-	    recognition.continuous = true;
+if ('webkitSpeechRecognition' in window) {
+	console.log("webkit is available!");
+	var recognition = new webkitSpeechRecognition();
+		recognition.continuous = true;
 
-	    recognition.onstart = function() {
-	      recognizing = true;
-	    };
+		recognition.onstart = function() {
+			recognizing = true;
+		};
 
-	    recognition.onerror = function(event) {
-	      console.log(event.error);
-	    };
+		recognition.onerror = function(event) {
+			console.log(event.error);
+		};
 
-	    recognition.onend = function() {
-	      recognizing = false;
-	    };
+		recognition.onend = function() {
+			recognizing = false;
+		};
 
-	    recognition.onresult = function(event) {
+		recognition.onresult = function(event) {
 
-				text = event.results[0][0].transcript;
-	      final_span.innerHTML = text;
+			const text = event.results[0][0].transcript;
+			final_span.innerHTML = text;
+		};
+}
 
-				Meteor.call("send_text_for_APIAI_processing", text, function(err, result){
-					if(err){
-						window.alert(err);
-						return;
-					}
-					console.log(result);
-				})
-	    };
+function startDictation(event) {
+	if (recognizing) {
+		recognition.stop();
+		var eventsave = new SpeechSynthesisUtterance('event is added to your calendar!');
+		window.speechSynthesis.speak(eventsave);
+		return;
 	}
+	final_transcript = '';
+	recognition.lang = 'en-US';
+	recognition.start();
+	final_span.innerHTML = '';
+}
 
-	function startDictation(event) {
-	  if (recognizing) {
-	    recognition.stop();
-	    return;
-	  }
-	  	final_transcript = '';
-	  	recognition.lang = 'en-US';
-	  	recognition.start();
-	  	final_span.innerHTML = '';
-	  	//interim_span.innerHTML = '';
-		}
-
-  Template.calendar.events({
-	'click #start_button': function(event){
-		startDictation(event);
-	},
-
+Template.calendar.events({
+'click #start_button': function(event){
+	startDictation(event);
+}
 });
