349387fa78b8acc266d612cfa07cb129b24ed7d0
Rong GUAN <tianbao971@gmail.com>
2017-07-19 13:42:31 -0400

    Make "petforum" working

---
+++ pages/Petforum/client/petforum.js
@@ -0,0 +1,38 @@
+Template.petforum.onCreated(function() {
+  Meteor.subscribe('post');
+  Meteor.subscribe('user');
+})
+
+Template.showpetpost.helpers({
+  petpostlist() {
+    return Post.find({field: "pet"})
+  },
+})
+
+Template.addpetpost.events({
+  'click button'(elt,instance) {
+    const petpostbox = instance.$('#petpostbox').val();
+    const name = User.findOne({owner: Meteor.userId()}).firstname + " " + User.findOne({owner: Meteor.userId()}).lastname;
+    console.log(petpostbox);
+
+    instance.$('#petpostbox').val("");
+    var posttext =
+      { postbox:petpostbox,
+        name:name,
+        field: "pet"
+      };
+    Meteor.call('post.insert', posttext);
+
+    // var msg = new SpeechSynthesisUtterance('message is sent!');
+    // window.speechSynthesis.speak(msg);
+  }
+})
+
+Template.petpostrow.events({
+  'click span'(elt,instance) {
+    console.dir(this);
+    var id = this.post._id
+    console.log(id);
+    Meteor.call('post.remove', id);
+  }
+})
