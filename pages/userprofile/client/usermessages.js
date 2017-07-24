5121eb5c4d07513b79710ed420d0ead4b09f25d8
YaxinGuo <yxguo0922@brandeis.edu>
2017-07-24 10:13:53 -0400

    send message

--- pages/userprofile/client/usermessages.js
+++ pages/userprofile/client/usermessages.js
@@ -1,30 +1,108 @@
 
-Template.messagerow.helpers({
-  "user": function(userid){
-    return User.findOne({owner:userid});
-  },
+Template.sendmessage.events({
+  'keypress #input': function (e, instance){
+     if (e.which == 13) {
+       console.log('enter key pressed')
+       //elt.preventDefault();
+       const messageStringKey = instance.$('#input').val();
+
+       function updateScrollKey(){
+         var element =instance.$("#input");
+         element.scrollTop = element.scrollHeight;
+       }
+
+       var messageDataKey = {
+         ids:[Router.current().params.query.userid,Router.current().params.query.userid2],
+         messagesArray: [
+           {
+             "message": input,
+             "author": Meteor.userId()
+           }
+         ]
+       };
+
+       instance.$('#input').val("");
+
+      if (Usermessages.findOne({ids:{$all:[Router.current().params.query.userid,Router.current().params.query.userid2]}})) {
+         console.log('updating message');
+         Meteor.call('usermessages.addmessage', [Router.current().params.query.userid,Router.current().params.query.userid2], messageDataKey.messagesArray,
+         function(error, result){
+           updateScrollKey();
+         });
+       } else { //else, add init message
+         Meteor.call('usermessages.addinitmessage', messageDataKey,
+         function(error, result){
+           updateScrollKey();
+         });
+         console.log('adding init message');
+       }
+     }
+     //console.log('enter key pressed');
+   },
+
+   'click button#send'(elt,instance) {
+      elt.preventDefault();
+      const input = instance.$('#input').val();
+      //const groupIdRef = this._id;
+
+      function updateScroll(){
+        var element = instance.$("#input");
+        element.scrollTop = element.scrollHeight;
+      }
+
+      var messageData = {
+        ids:[Router.current().params.query.userid,Router.current().params.query.userid2],
+        messagesArray: [
+          {
+            "message": input,
+            "author": Meteor.userId()
+          }
+        ]
+      };
+
+      instance.$('#input').val("");
+
+        //if Usermessages collection exists for the group add message
+
+        if (Usermessages.findOne({ids:{$all:[Router.current().params.query.userid,Router.current().params.query.userid2]}})) {
+          console.log('updating message');
+          Meteor.call('usermessages.addmessage', [Router.current().params.query.userid,Router.current().params.query.userid2], messageData.messagesArray);
+        } else { //else, add init message
+          Meteor.call('usermessages.addinitmessage', messageData);
+          console.log('adding init message');
+        }
+        console.log('Usermessages findOne: ');
+        console.dir(Usermessages.findOne({ids:{$all:[Router.current().params.query.userid,Router.current().params.query.userid2]}}));
+    }
 })
+
+
+Template.messagehistory.onRendered(function() {
+  var element = instance.$("#input");
+  //var element = instance.$('.messageText');
+  element.scrollTop = element.scrollHeight;
+});
+
+
 
 Template.messagehistory.helpers({
-  usermessageslist() {return Usermessages.find()},
+  showingMessages() {return Usermessages.find()},
 })
 
-Template.sendmessage.events({
-  'click button#send'(elt,instance) {
-    const time = new Date();
-    const text = instance.$('.text').val();
-    const ids = Router.current().params.query;
-    var usermessages = {ids:ids,
-                        time:time,
-                        text:text,
-                        author:Meteor.userId()}
 
-    Meteor.call('usermessages.insert',usermessages, function(err, result){
-          if(err){
-              alert(err.message);
-              return;
-          }
-    });
-    console.log(Usermessages.find());
-  }
+
+
+Template.messagerow.helpers({
+  getUsername(thisid) {
+    var profile = User.findOne({owner: thisid});
+    var fname = profile.firstname;
+    var lname = profile.lastname;
+    var personname = fname+' '+lname;
+    return personname;
+  },
+  getUserpic(thisid){
+    var profile = User.findOne({owner: thisid});
+    var pic = profile.pic;
+    return pic;
+  },
 })
