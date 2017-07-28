2d6e1f02dd1610aef340e09373bd5e6b21cb23d2
JAlva7 <JoshuaAlva7@gmail.com>
2017-07-28 10:25:10 -0400

    Began adding search feature to forum page.

--- pages/forums/client/publishedforums.js
+++ pages/forums/client/publishedforums.js
@@ -1,3 +1,14 @@
+Template.connections.onCreated( function(){
+    this.forumDict = new ReactiveDict();
+});
+
+Template.publishedforums.helpers({
+  searchlist: function() {
+     return Template.instance().forumDict.get('searchList');
+    // Must put objects into array to display properly!
+  },
+});
+
 Template.forumsrow.helpers({
   showingForums() {
     var forums=Forums.find().fetch();
@@ -11,6 +22,16 @@ Template.forumsrow.helpers({
     var name = fname + " " + lname;
     return name;
   }
+})
 
-
+Template.publishedforums.events({
+  "click #submit"(event, instance){
+    const query = instance.$('#search_id').val();
+    const regex = new RegExp(query, "i");
+    console.log(regex);
+    // undefined
+    Template.instance().userDict.set('searchList', User.find({fullname:regex}, {sort: {lastname: 1}}).fetch());
+    // Returns documents matching. How so?
+    console.log(Template.instance().userDict.get('searchList'));
+  },
 })
