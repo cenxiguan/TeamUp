e3861c6854f167bdb7d468cfb204d95c285c1038
Rong GUAN <tianbao971@gmail.com>
2017-08-01 23:26:11 -0400

    Get rid of not necessary function

--- pages/calendar/server/methods.js
+++ pages/calendar/server/methods.js
@@ -16,91 +16,91 @@ Meteor.methods({
       })
     },
 
-    "sned_wav_files_to_Google_Speech_API": function(){
-        const fs = Npm.require("fs");
-        const dirname = Meteor.settings.voice_input_path;
-        RecognitionResults.remove({status: {$exists: false}});
-
-        //save all path's of WAV files to an array
-        var wavPaths = [];
-        for(filename of fs.readdirSync(dirname)){
-            var extension = filename.substring(filename.lastIndexOf(".") + 1);
-            if(extension.toUpperCase() === "WAV"){
-              wavPaths.push(dirname + filename);
-            }
-        }
-
-        //check is there is any WAV file to submit
-        if(wavPaths.length == 0){
-          throw new Meteor.Error(301, "No WAV file found");
-        }
-
-        var count = 0;
-        for(filePath of wavPaths){
-            //make sync calls to google
-            const Speech = Npm.require('@google-cloud/speech');
-            const speech = Speech();
-            const options = {
-              encoding: 'LINEAR16',
-              //sampleRateHertz: 48000,
-              languageCode: 'en-US'
-            };
-
-            (function(filePathVar){
-              speech.recognize(filePathVar, options)
-              .then((result) => {
-                count++;
-
-                //when all the files have been processed
-                const object_id = RecognitionResults.insert({
-                  audioFile: filePathVar,
-                  result: result
-                })
-
-                if(count == wavPaths.length){
-                  if(!RecognitionResults.findOne({status: {$exists: true}})){
-                    RecognitionResults.insert({
-                      status: "done"
-                    })
-                  } else {
-                    RecognitionResults.update({
-                      status: {$exists: true}
-                    }, {
-                      status: "done"
-                    });
-                  }
-                }
-
-                //send the result to API.ai
-                Meteor.call("send_text_for_APIAI_processing", result[0], function(err, APIresult){
-                  if(err){
-                    console.log(err);
-                  } else {
-                    //get the intent
-                    const intentName = APIresult.data.result.metadata.intentName;
-
-                    //get the entities
-                    const entities = [];
-                    const parameters = APIresult.data.result.parameters
-                    for(entity in parameters){
-                      if(parameters[entity]){
-                        entities.push(entity);
-                      }
-                    }
-
-                    //save the result to the database
-                    RecognitionResults.update(object_id, {$push: {
-                      intentName: intentName,
-                      entities: entities
-                    }})
-                  }
-                })
-              })
-              .catch((err) => {
-                console.error('ERROR:', err);
-                return err;
-              })
-            })(filePath);
-        }
-    },
+    // "sned_wav_files_to_Google_Speech_API": function(){
+    //     const fs = Npm.require("fs");
+    //     const dirname = Meteor.settings.voice_input_path;
+    //     RecognitionResults.remove({status: {$exists: false}});
+    //
+    //     //save all path's of WAV files to an array
+    //     var wavPaths = [];
+    //     for(filename of fs.readdirSync(dirname)){
+    //         var extension = filename.substring(filename.lastIndexOf(".") + 1);
+    //         if(extension.toUpperCase() === "WAV"){
+    //           wavPaths.push(dirname + filename);
+    //         }
+    //     }
+    //
+    //     //check is there is any WAV file to submit
+    //     if(wavPaths.length == 0){
+    //       throw new Meteor.Error(301, "No WAV file found");
+    //     }
+    //
+    //     var count = 0;
+    //     for(filePath of wavPaths){
+    //         //make sync calls to google
+    //         const Speech = Npm.require('@google-cloud/speech');
+    //         const speech = Speech();
+    //         const options = {
+    //           encoding: 'LINEAR16',
+    //           //sampleRateHertz: 48000,
+    //           languageCode: 'en-US'
+    //         };
+    //
+    //         (function(filePathVar){
+    //           speech.recognize(filePathVar, options)
+    //           .then((result) => {
+    //             count++;
+    //
+    //             //when all the files have been processed
+    //             const object_id = RecognitionResults.insert({
+    //               audioFile: filePathVar,
+    //               result: result
+    //             })
+    //
+    //             if(count == wavPaths.length){
+    //               if(!RecognitionResults.findOne({status: {$exists: true}})){
+    //                 RecognitionResults.insert({
+    //                   status: "done"
+    //                 })
+    //               } else {
+    //                 RecognitionResults.update({
+    //                   status: {$exists: true}
+    //                 }, {
+    //                   status: "done"
+    //                 });
+    //               }
+    //             }
+    //
+    //             //send the result to API.ai
+    //             Meteor.call("send_text_for_APIAI_processing", result[0], function(err, APIresult){
+    //               if(err){
+    //                 console.log(err);
+    //               } else {
+    //                 //get the intent
+    //                 const intentName = APIresult.data.result.metadata.intentName;
+    //
+    //                 //get the entities
+    //                 const entities = [];
+    //                 const parameters = APIresult.data.result.parameters
+    //                 for(entity in parameters){
+    //                   if(parameters[entity]){
+    //                     entities.push(entity);
+    //                   }
+    //                 }
+    //
+    //                 //save the result to the database
+    //                 RecognitionResults.update(object_id, {$push: {
+    //                   intentName: intentName,
+    //                   entities: entities
+    //                 }})
+    //               }
+    //             })
+    //           })
+    //           .catch((err) => {
+    //             console.error('ERROR:', err);
+    //             return err;
+    //           })
+    //         })(filePath);
+    //     }
+    // },
 })
